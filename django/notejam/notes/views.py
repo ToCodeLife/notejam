4e9fc337839aa7adbaf8cf65220d2402b1b6e9ca
ToCodeLife <jsonlim67889@gmail.com>
2013-03-11 22:50:41 +0200

    Added pad column to main table

--- django/notejam/notes/views.py
+++ django/notejam/notes/views.py
@@ -11,10 +11,7 @@ from notes.forms import NoteForm
 class NoteCreateView(CreateView):
     model = Note
     form_class = NoteForm
-    template_name_suffix = '_create'
-
-    def get_initial(self):
-        return {'pad': self.request.GET.get('pad', None)}
+    success_url = reverse_lazy('home')
 
     def form_valid(self, form):
         self.object = form.save(commit=False)
@@ -22,20 +19,11 @@ class NoteCreateView(CreateView):
         self.object.save()
         return redirect(self.get_success_url())
 
-    def get_success_url(self):
-        if self.object.pad is not None:
-            return reverse_lazy(
-                'view_pad_notes', kwargs={'pk': self.object.pad.id}
-            )
-        else:
-            return reverse_lazy('home')
-
 
 class NoteUpdateView(UpdateView):
     model = Note
     form_class = NoteForm
     success_url = reverse_lazy('home')
-    template_name_suffix = '_edit'
 
 
 class NoteDeleteView(DeleteView):
