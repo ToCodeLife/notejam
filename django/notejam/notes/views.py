3821ba9c07d6aa05ea16947a0f369e20950f3dce
ToCodeLife <jsonlim67889@gmail.com>
2013-03-18 13:32:40 +0200

    Merge branch 'html'

--- django/notejam/notes/views.py
+++ django/notejam/notes/views.py
@@ -4,14 +4,25 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.detail import DetailView
 from django.views.generic import ListView
 
-from notes.models import Note
+from notes.models import Note, Pad
 from notes.forms import NoteForm
 
 
 class NoteCreateView(CreateView):
     model = Note
     form_class = NoteForm
-    success_url = reverse_lazy('home')
+    template_name_suffix = '_create'
+
+    def get_initial(self):
+        return {'pad': self.request.GET.get('pad', None)}
+
+    def get_form(self, form_class):
+        form = super(NoteCreateView, self).get_form(self.get_form_class())
+        # limit pad choice
+        form.fields['pad'].queryset = Pad.objects.filter(
+            user=self.request.user
+        )
+        return form
 
     def form_valid(self, form):
         self.object = form.save(commit=False)
@@ -19,27 +30,64 @@ class NoteCreateView(CreateView):
         self.object.save()
         return redirect(self.get_success_url())
 
+    def get_success_url(self):
+        if self.object.pad is not None:
+            return reverse_lazy(
+                'view_pad_notes', kwargs={'pk': self.object.pad.id}
+            )
+        else:
+            return reverse_lazy('home')
+
 
 class NoteUpdateView(UpdateView):
     model = Note
     form_class = NoteForm
     success_url = reverse_lazy('home')
+    template_name_suffix = '_edit'
+
+    def get_queryset(self):
+        qs = super(NoteUpdateView, self).get_queryset()
+        return qs.filter(user=self.request.user)
+
+    def get_success_url(self):
+        if self.object.pad is not None:
+            return reverse_lazy(
+                'view_pad_notes', kwargs={'pk': self.object.pad.id}
+            )
+        else:
+            return reverse_lazy('home')
 
 
 class NoteDeleteView(DeleteView):
     model = Note
 
+    def get_queryset(self):
+        qs = super(NoteDeleteView, self).get_queryset()
+        return qs.filter(user=self.request.user)
+
+    def get_success_url(self):
+        if self.object.pad is not None:
+            return reverse_lazy(
+                'view_pad_notes', kwargs={'pk': self.object.pad.id}
+            )
+        else:
+            return reverse_lazy('home')
+
 
 class NoteDetailView(DetailView):
     model = Note
+
+    def get_queryset(self):
+        qs = super(NoteDetailView, self).get_queryset()
+        return qs.filter(user=self.request.user)
 
 
 class NoteListView(ListView):
     model = Note
     context_object_name = 'notes'
-    paginate_by = 10
     order_by = '-updated_at'
 
     def get_queryset(self):
+        qs = super(NoteListView, self).get_queryset()
         order_by = self.request.GET.get('order', self.order_by)
-        return Note.objects.filter().order_by(order_by)
+        return qs.filter(user=self.request.user).order_by(order_by)
