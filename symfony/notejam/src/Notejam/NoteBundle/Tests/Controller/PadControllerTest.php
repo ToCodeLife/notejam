5bcab96de69db187253a740d6ddbca028250bd59
ToCodeLife <jsonlim67889@gmail.com>
2014-03-05 15:33:59 +0100

    Symfony: Authentication while testing

---
+++ symfony/notejam/src/Notejam/NoteBundle/Tests/Controller/PadControllerTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Notejam\NoteBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class PadControllerTest extends WebTestCase
+{
+    public function testCreatePadSuccess()
+    {
+    }
+
+    public function testCreatePadErrorRequiredFields() 
+    {
+    }
+
+    public function testEditPadSuccess() 
+    {
+    }
+
+    public function testEditPadErrorRequiredFields() 
+    {
+    }
+
+    public function testDeletePadSuccess()
+    {
+    }
+
+    public function testDeletePadErrorAnotherUser()
+    {
+    }
+
+    public function testViewPadSuccess()
+    {
+    }
+}
+
+
