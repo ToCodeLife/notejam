26d3bcccbbae0ffa755b1d69d2233e975cbe068e
Markus Malkusch <markus@malkusch.de>
2015-11-19 00:11:42 +0100

    Spring: Fixes ambigous i18n key Email

---
+++ spring/src/main/java/net/notejam/spring/Application.java
@@ -0,0 +1,27 @@
+package net.notejam.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+
+/**
+ * Configures the Spring framework
+ *
+ * @author markus@malkusch.de
+ * @see <a href="bitcoin:1335STSwu9hST4vcMRppEPgENMHD2r1REK">Donations</a>
+ */
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+	
+	@Bean
+	public LocaleResolver localeResolver() {
+		return new AcceptHeaderLocaleResolver();
+	}
+	
+}
